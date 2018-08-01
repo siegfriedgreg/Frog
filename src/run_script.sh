eb6e6d3188748d04f1f1b80eacfb53de452e9c01
Greg Siegfried <40606199+siegfriedgreg@users.noreply.github.com>
2018-08-01 14:19:57 -0400

    Update run_script.sh

--- src/run_script.sh
+++ src/run_script.sh
@@ -4,8 +4,8 @@
 #SBATCH -t 1:00:00
 #SBATCH --gres=gpu:k80:4
 
-# ./exp ../[graph location]/***.mtx.bin                   <-- SMALL GRAPH SIZES
-# ./exp ../[graph location]/***.mtx.bin [buffer size]     <-- LARGE GRAPH SIZES
+#   ./exp ../[graph location]/***.mtx.bin                   <-- SMALL GRAPH SIZES
+#   ./exp ../[graph location]/***.mtx.bin [buffer size]     <-- LARGE GRAPH SIZES
 
 #   ./exp ../../[graph location]/***.mtx.bin                  <-- SMALL RUN
 #   ./exp ../../[graph location]/***.mtx.bin 1048576          <-- LARGE RUN
